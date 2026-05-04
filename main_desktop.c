836c66af38e54a2bc009fd2d5ac2f3e427569bae
zz6zz666 <1807651273@qq.com>
2026-05-04 23:43:40 +0800

    feat: disable Windows DPI scaling for 1:1 pixel rendering via SetProcessDPIAware

--- main_desktop.c
+++ main_desktop.c
@@ -475,6 +475,7 @@ int main(int argc, char **argv)
 {
 #if defined(PLATFORM_WINDOWS)
     SetConsoleOutputCP(CP_UTF8);
+    SetProcessDPIAware();
     SetConsoleCtrlHandler(win_ctrl_handler, TRUE);
     signal(SIGINT, sig_handler);
     signal(SIGTERM, sig_handler);
